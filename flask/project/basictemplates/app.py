415fcf1ebfceaaba729a1584a936106e917a8a39
Laura B <lmcarcione@gmail.com>
2021-02-13 20:21:55 -0800

    Example moved to its own folder

---
+++ flask/project/basictemplates/app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 18:35:54 2021
+
+@author: laura
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+
+
+def index():
+    return render_template('index.html',current_title='My Task Manager')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
