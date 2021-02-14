5bf42d4fc424060db6594702f4fad3c46083a14a
Laura B <lmcarcione@gmail.com>
2021-02-13 19:36:09 -0800

    Passing argument to html template file

--- flask/project/app.py
+++ flask/project/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_title='My Task Manager')
 
 
 if __name__ == '__main__':
