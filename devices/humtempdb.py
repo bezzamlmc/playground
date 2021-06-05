4f7b8345bed9a6b4b1346d7e026d431728587380
Laura B <lmcarcione@gmail.com>
2021-06-04 18:08:37 -0700

    Sensor DB added WIP

---
+++ devices/humtempdb.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 18:07:51 2021
+
+@author: laura
+"""
+
+import sqlite3
+from sqlite3 import Error
+
+class HumTempDB:
+    def __init__(self,database):
+        self.database = database
+        self.conn = None
+        self.lastId = 0
+        self.setup_table()
+        
+    def setup_table(self):
+        sql_create_samples_table = """CREATE TABLE IF NOT EXISTS samples (
+                                        id integer PRIMARY KEY,
+                                        timestamp text,
+                                        humidity integer,
+                                        temperature integer); """
+        # create a database connection
+        self.conn = self.create_connection()
+        # create tables
+        if self.conn is not None:
+            # create samples table
+            self.create_table(sql_create_samples_table)
+            self.lastId = self.total_samples()
+        else:
+            print("Error! cannot create the database connection.")
+        
+    def create_connection(self):
+        """ create a database connection to a SQLite database """
+        conn = None
+        try:
+            conn = sqlite3.connect(self.database)
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+        return conn
+
+    def create_table(self,create_table_sql):
+        """ create a table from the create_table_sql statement
+        :param conn: Connection object
+        :param create_table_sql: a CREATE TABLE statement
+        :return:
+        """
+        try:
+            c = self.conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
+        
+        
+    def insert_record(self,timestamp, humidity, temperature):
+        """ Insert an id, timestamp, humidity, temperature record"""
+        self.lastId  = self.lastId + 1
+
+        sql = """INSERT INTO samples(seld.lastId,timestamp,humidity,temperature) 
+                VALUES(?, ?, ?,?) """
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        self.conn.commit()
+        return cur.lastrowid
+
+    def total_samples(self):
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM samples")
+        results = cur.fetchall()
+        return len(results)
+
+    def query_samples(self,lastn):
+        """Retrieve the last n samples"""
+        cur = self.conn.cursor()  
+        cur.execute("SELECT * FROM samples ORDER BY id DESC LIMIT ? ", (lastn))
+        rows = cur.fetchall()    
+        return rows
+    
+    def shutdown(self):
+        self.conn.commit()
+        self.conn.close()
+
+if __name__ == '__main__':
+    database = r"C:\Users\laura\work\db\humtemp.db"
+    db = HumTempDB(database)
--- devices/sensor.py
+++ devices/sensor.py
@@ -9,20 +9,51 @@ import sys
 import os
 from PyQt5.QtWidgets import QDialog, QApplication
 from humtemp import Ui_sensorDialog as sensorDialog
+from pseudoSensor import PseudoSensor
+from humtempdb import HumTempDB
 
 class SensorWindow(QDialog):
-    def __init__(self):
+    def __init__(self, db):
         super().__init__()
         self.ui = sensorDialog()
         self.ui.setupUi(self)
-        self.show()
+        
+        self.db = db
+# Some settings
         self.unit = 0
         self.unitConv = 1
+# Connections added
+        self.ui.read1PB.clicked.connect(self.slot_read1)
+        self.ui.read10PB.clicked.connect(self.slot_read10)
+        self.ui.statsPB.clicked.connect(self.slot_stats)
+        self.ui.displayPB.clicked.connect(self.slot_display)
+        self.ui.displayPB.clicked.connect(self.slot_quit)
+# Show it!
         self.show()
         
+    def slot_read1(self):
+        h,t = ps.generate_values()
+        print("H ",h)
+        print("T ",t)
+        
+    def slot_read10(self):
+        print("read1")
+        
+    def slot_stats(self):
+        print("stats")
+        
+    def slot_display():
+        print("display")
+        
+    def slot_quit(self):
+        db.shutdown()
+        self.ui_quitPB.close()
+        
 if __name__ == '__main__':
+    database = r"C:\Users\laura\work\db\humtemp.db"
+    db = HumTempDB(database)
     os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
     app = QApplication(sys.argv)
-    w = SensorWindow()
+    w = SensorWindow(db)
     w.show()
     sys.exit(app.exec_())
