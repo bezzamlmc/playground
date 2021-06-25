53ef5d4d2d6989d4ca3b603b06089b0a628341f5
Laura B <lmcarcione@gmail.com>
2021-06-25 14:38:35 -0700

    Restructured to isolate mains

--- go/coursera/ex1/trunc.go
+++ go/coursera/ex1/trunc.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import {
+	"fmt"
+}
 
 func main() {
 	var f1 float32
 
 	fmt.Printf("Enter a floating point number:\n")
+	f1 = 8.2
 	fmt.Scanf("%f", &f1)
 
 	var if1 int32 = int32(f1)
